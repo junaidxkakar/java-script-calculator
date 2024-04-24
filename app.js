a095327b4e0075a0bde3639a9d6dd28f9f5f2dc8
junaidxkakar <jkkakar12345@gmail.com>
2024-04-24 08:33:53 -0700

    first work in js

---
+++ app.js
@@ -0,0 +1,40 @@
+// alert("hello world")
+// var abc = "junaid khan"
+// alert(abc)
+// var a = prompt("value1")
+// var b = prompt("value2")
+// document.write(a-b+a*b+a/b)
+
+
+var num1 = +prompt('Enter value 1')
+var num2 = +prompt('Enter value 2')
+var add = num1+num2
+document.write(
+    '<table border="1px" >'+'<tr>'+'</tr>'+
+    '<th>'+"ADDITION"+'</th>'+
+    "<tr>"+"<td>" +add+ "</td>"+
+    "</tr>"+
+
+    "</table>"
+
+)
+var sub = num1-num2
+document.write(
+    '<table border="1px" >'+'<tr>'+'</tr>'+
+    '<th>'+"subtriction"+'</th>'+
+    "<tr>"+"<td>" +sub+ "</td>"+
+    "</tr>"+
+
+    "</table>"
+    
+)
+    var mul = num1*num2
+document.write(
+    '<table border="1px" >'+'<tr>'+'</tr>'+
+    '<th>'+"MULTIFICATIN"+'</th>'+
+    "<tr>"+"<td>" +mul+ "</td>"+
+    "</tr>"+
+
+    "</table>"
+    
+)
